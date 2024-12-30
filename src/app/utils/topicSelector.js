25e4eb8e224f05b228d8da917599ee4077083343
sumitabha4 <sumitabha4@gmail.com>
2024-12-30 18:18:56 +0000

    topicSelector will return userimage too

--- src/app/utils/topicSelector.js
+++ src/app/utils/topicSelector.js
@@ -1,11 +1,19 @@
 import users from '../../../data/users.json';
 
 export function selectTopics(count = 5) {
-    const shuffled = [...users].sort(() => 0.5 - Math.random());
+    // Filter users to only include those with isAi: true
+    const aiUsers = users.filter(user => user.isAi);
+
+    // Shuffle the filtered users
+    const shuffled = [...aiUsers].sort(() => 0.5 - Math.random());
+
+    // Select the required number of users
     const selected = shuffled.slice(0, count);
- 
-    return selected.reduce((acc, user) => {
-        acc[user.username] = true;
-        return acc;
-    }, {});
+
+    // Return array of objects with id, username, and image
+    return selected.map(user => ({
+        id: user.id,
+        username: user.username,
+        image: user.image,
+    }));
 }
