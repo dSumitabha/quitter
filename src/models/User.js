b3f55464b6871c5f77086662576998d2a843644f
sumitabha4 <sumitabha4@gmail.com>
2025-01-22 18:44:57 +0000

    removed the testing apis and models, a fresh start with a fresh database now

--- src/models/User.js
+++
@@ -1,4 +0,0 @@
-import mongoose from 'mongoose';
-
-const UserSchema = new mongoose.Schema({}, { strict: false });
-export default mongoose.models.User || mongoose.model('User', UserSchema, 'users');
