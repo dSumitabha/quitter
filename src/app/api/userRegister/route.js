3d7f8502177701262f42909208219960a229fde9
sumitabha4 <sumitabha4@gmail.com>
2025-01-25 19:15:31 +0000

    bcrypt used

--- src/app/api/userRegister/route.js
+++ src/app/api/userRegister/route.js
@@ -1,6 +1,7 @@
 // app/api/register/route.js
 import { NextResponse } from 'next/server';
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs'
 import User from '@/models/User';
 import connectDB from '@/lib/db';
 
@@ -11,7 +12,7 @@ export async function POST(request) {
     const { username, email, password } = await request.json();
 
     // Check if user already exists
-    const existingUser = await User.findOne({ 
+    const existingUser = await User.findOne({
       $or: [{ email }, { username }] 
     });
 
@@ -22,11 +23,13 @@ export async function POST(request) {
       );
     }
 
+    const hashedPassword = await bcrypt.hash(password, 10)
+
     // Create new user
     const newUser = new User({
       username,
       email,
-      password, // Note: In production, hash this password
+      password : hashedPassword, // Note: In production, hash this password
       role: 'user' // Default role
     });
 
