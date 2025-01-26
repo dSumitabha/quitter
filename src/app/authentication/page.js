c0a0344bbf8d6c413037688b5cf2e5af2984c8d2
sumitabha4 <sumitabha4@gmail.com>
2025-01-26 17:03:00 +0000

    authentication route created

---
+++ src/app/authentication/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Register from '../components/register';
+
+const Authentication = () => {
+  return (
+    <div className="max-w-md mx-auto mt-4">
+      <Register />
+    </div>
+  );
+};
+
+export default Authentication;
