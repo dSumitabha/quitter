ea12f9b63c82fc4a7673cb737be60cf8b92c0cf0
dSumitabha <sumitabha4@gmail.com>
2025-05-08 11:43:43 +0530

    refactor: rename ProfilePage to CreatePostPage and simplify CreatePost component usage

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -3,7 +3,7 @@ import Register from '../components/Register';
 import Profile from '../components/Profile';
 import "../globals.css";
 
-export default async function ProfilePage() {
+export default async function CreatePostPage() {
 
   return <Profile  />;
 }
