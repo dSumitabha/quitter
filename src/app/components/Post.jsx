fbbf77e2168ea16f78bd0cde7ec4c03652fffd16
dSumitabha <sumitabha4@gmail.com>
2025-05-16 15:55:46 +0530

    refactor: enhance dark mode support in Post component styles

--- src/app/components/Post.jsx
+++ src/app/components/Post.jsx
@@ -35,19 +35,19 @@ const Post = ({ postId, username, content, likes, createdAt, image, bio, isNew, 
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div className="p-4 border-b border-gray-200 my-2 bg-white shadow-sm">
+    <div className="p-4 border-b border-gray-200 dark:border-slate-800 my-2 bg-white dark:bg-slate-950 shadow-sm">
         <div className="flex items-center mb-2">
             <Image src={`/avatar/${image}`} alt={`${username}'s profile`} width={40} height={40} className="w-10 h-10 rounded-full mr-3" />
             <div>
-                <Link href={`${username}`} className="font-semibold text-gray-700 flex items-center" title={bio}>
+                <Link href={`${username}`} className="font-semibold text-gray-700 dark:text-slate-300 flex items-center" title={bio}>
                     {username}
                     {isNew && <span className="text-yellow-500 ml-2" title={image}>✨</span>}
                 </Link>
-                <div className="relative inline-block text-xs text-gray-500" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} >
+                <div className="relative inline-block text-xs text-gray-500 dark:text-slate-500" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} >
                     <p className="text-xs text-gray-500" > {formattedDate(createdAt)} </p>
                     {isHovered && (
                         <div className="absolute top-6 left-3/4  transform -translate-x-1/2 mb-2 z-10">
-                            <div className="bg-gray-800 text-white  text-xs rounded-md px-3 py-1 shadow-lg relative w-max">
+                            <div className="bg-slate-800 text-white text-xs rounded-md px-3 py-1 shadow-lg relative w-max">
                                 {dayjs(createdAt).format('DD-MM-YYYY · hh:mm A')}
                                 <div className="absolute -top-1 left-1/3 transform -translate-x-1/2 w-2 h-2 bg-gray-800  rotate-45"></div>
                             </div>
@@ -56,7 +56,7 @@ const Post = ({ postId, username, content, likes, createdAt, image, bio, isNew, 
                 </div>
             </div>
         </div>
-        <p className="text-gray-800 mb-2">{content}</p>
+        <p className="text-slate-800 dark:text-slate-200 mb-2">{content}</p>
         <div className="text-sm text-gray-500">
             <LikeButton postId={postId} initialLikes={likes} isInitiallyLiked={isLiked} onLikeChange={handleLikeChange} />
         </div>
