8c667c77426562b4ace7ebcdae521a99b7316a58
sumitabha4 <sumitabha4@gmail.com>
2025-05-17 18:02:24 +0000

    dark theme updated for login and register components

--- src/app/components/Login.jsx
+++ src/app/components/Login.jsx
@@ -7,11 +7,11 @@ const Login = ({ handleChange, formData, handleSubmit }) => {
       <div className="rounded-md shadow-sm space-y-4">
         <div>
           <label htmlFor="username" className="sr-only">Username</label>
-          <input id="username" name="username" type="text" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" placeholder="Username" value={formData.username} onChange={handleChange}/>
+          <input id="username" name="username" type="text" required className="appearance-none rounded-none relative block w-full px-3 py-2 dark:bg-slate-700 border border-gray-300 dark:border-gray-600 placeholder-gray-600 dark:placeholder-gray-400 text-gray-900 dark:text-white rounded-t-md focus:outline-none focus:ring-blue-700 dark:focus:ring-blue-300 focus:border-blue-600 dark:focus:border-blue-400 focus:z-10 sm:text-sm" placeholder="Username" value={formData.username} onChange={handleChange}/>
         </div>
         <div>
           <label htmlFor="password" className="sr-only">Password</label>
-          <input id="password" name="password" type="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" placeholder="Password" value={formData.password} onChange={handleChange}/>
+          <input id="password" name="password" type="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 dark:bg-slate-700 border border-gray-300 dark:border-gray-600 placeholder-gray-600 dark:placeholder-gray-400 text-gray-900 dark:text-white rounded-b-md focus:outline-none focus:ring-blue-500 dark:focus:ring-blue-300 focus:border-blue-600 dark:focus:border-blue-400 focus:z-10 sm:text-sm" placeholder="Password" value={formData.password} onChange={handleChange}/>
         </div>
       </div>
 
--- src/app/components/Register.jsx
+++ src/app/components/Register.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react';
+import React from 'react';
 
 const Register = ({ handleChange, formData, handleSubmit }) => {
   return (
@@ -7,19 +7,19 @@ const Register = ({ handleChange, formData, handleSubmit }) => {
       <div className="rounded-md shadow-sm space-y-4">
         <div>
           <label htmlFor="username" className="sr-only">Username</label>
-          <input id="username" name="username" type="text" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" placeholder="Username" value={formData.username} onChange={handleChange} />
+          <input id="username" name="username" type="text" required className="appearance-none rounded-none relative block w-full px-3 py-2 dark:bg-slate-700 border border-gray-300 dark:border-gray-600 placeholder-gray-600 dark:placeholder-gray-400 text-gray-900 dark:text-white rounded-t-md focus:outline-none focus:ring-blue-700 dark:focus:ring-blue-300 focus:border-blue-600 dark:focus:border-blue-400 focus:z-10 sm:text-sm" placeholder="Username" value={formData.username} onChange={handleChange}/>
         </div>
         <div>
           <label htmlFor="email" className="sr-only">Email address</label>
-          <input id="email" name="email" type="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" placeholder="Email address" value={formData.email} onChange={handleChange} />
+          <input id="email" name="email" type="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 dark:bg-slate-700 border border-gray-300 dark:border-gray-600 placeholder-gray-600 dark:placeholder-gray-400 text-gray-900 dark:text-white focus:outline-none focus:ring-blue-700 dark:focus:ring-blue-300 focus:border-blue-600 dark:focus:border-blue-400 focus:z-10 sm:text-sm" placeholder="Email address" value={formData.email} onChange={handleChange}/>
         </div>
         <div>
           <label htmlFor="password" className="sr-only">Password</label>
-          <input id="password" name="password" type="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" placeholder="Password" value={formData.password} onChange={handleChange} />
+          <input id="password" name="password" type="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 dark:bg-slate-700 border border-gray-300 dark:border-gray-600 placeholder-gray-600 dark:placeholder-gray-400 text-gray-900 dark:text-white focus:outline-none focus:ring-blue-700 dark:focus:ring-blue-300 focus:border-blue-600 dark:focus:border-blue-400 focus:z-10 sm:text-sm" placeholder="Password" value={formData.password} onChange={handleChange} />
         </div>
         <div>
           <label htmlFor="confirmPassword" className="sr-only">Confirm Password</label>
-          <input id="confirmPassword" name="confirmPassword" type="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" placeholder="Confirm Password" value={formData.confirmPassword} onChange={handleChange} />
+          <input id="confirmPassword" name="confirmPassword" type="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 dark:bg-slate-700 border border-gray-300 dark:border-gray-600 placeholder-gray-600 dark:placeholder-gray-400 text-gray-900 dark:text-white rounded-b-md focus:outline-none focus:ring-blue-700 dark:focus:ring-blue-300 focus:border-blue-600 dark:focus:border-blue-400 focus:z-10 sm:text-sm" placeholder="Confirm Password" value={formData.confirmPassword} onChange={handleChange}/>
         </div>
       </div>
 
