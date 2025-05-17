0986550f0bb56ea1c8f42432e183dcbeb8bd5c03
sumitabha4 <sumitabha4@gmail.com>
2025-05-17 18:09:23 +0000

    create post component upgraded for dark theme

--- src/app/components/CreatePost.jsx
+++ src/app/components/CreatePost.jsx
@@ -45,16 +45,16 @@ const CreatePost = ({isAuthenticated}) => {
 
   return (
     <div className="max-w-md mx-auto mt-4">
-      <div className="bg-white w-full max-w-md p-6 rounded shadow-md">
-        <h2 className="text-2xl font-bold text-gray-800 mb-4 text-center">Create a Post</h2>
+      <div className="bg-white dark:bg-slate-950 w-full max-w-md p-6 rounded shadow-md">
+        <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-200 mb-4 text-center">Create a Post</h2>
         <form onSubmit={handleSubmit}>
-          <textarea className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent resize-none h-32 text-sm dark:text-neutral-800" placeholder="What's on your mind?" value={content} onChange={(e) => setContent(e.target.value)} maxLength={280} />
+          <textarea className="w-full dark:bg-slate-800 p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent resize-y min-h-32 text-sm text-neutral-800 dark:text-neutral-200 placeholder-neutral-500 dark:placeholder-neutral-500" placeholder="What's on your mind?" value={content} onChange={(e) => setContent(e.target.value)} maxLength={280} />
           {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
           <button type="submit" disabled={submitting} className="w-full mt-4 bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition disabled:opacity-50" >
             {submitting ? 'Posting...' : 'Post'}
           </button>
         </form>
-        {!isAuthenticated && <small className="my-4 text-red-600">You aren't logged in. <Link href="/authentication" className="text-blue-700">Login </Link></small>}
+        {!isAuthenticated && <small className="my-4 text-red-600 dark:text-red-500">You aren't logged in. <Link href="/authentication" className="text-blue-700 hover:text-blue-500">Login </Link></small>}
       </div>
     </div>
   );
